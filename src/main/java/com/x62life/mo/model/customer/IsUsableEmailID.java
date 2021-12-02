21bff34fadfd91e8dcb01ba0ab34428a3385a29a
davinci000777 <Yckim2001@naver.com>
2021-12-02 13:05:13 +0900

    util - usableEmailId 추가

---
+++ src/main/java/com/x62life/mo/model/customer/IsUsableEmailID.java
@@ -0,0 +1,26 @@
+package com.x62life.mo.model.customer;
+
+public class IsUsableEmailID {
+
+	private String resultCode;
+	private String resultMessage;
+	
+	public String getResultCode() {
+		return resultCode;
+	}
+
+
+	public void setResultCode(String resultCode) {
+		this.resultCode = resultCode;
+	}
+
+
+	public String getResultMessage() {
+		return resultMessage;
+	}
+
+
+	public void setResultMessage(String resultMessage) {
+		this.resultMessage = resultMessage;
+	}
+}
