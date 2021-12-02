33c039adcf88286f78f426e7048dd97798bdb7a1
davinci000777 <Yckim2001@naver.com>
2021-12-02 10:08:40 +0900

    util - making 추가

---
+++ src/main/java/com/x62life/mo/common/util/masking/AbstractMasker.java
@@ -0,0 +1,10 @@
+package com.x62life.mo.common.util.masking;
+
+public class AbstractMasker implements Masker {
+
+    @Override
+    public String mask(String value) {
+        return value;
+    }
+
+}
---
+++ src/main/java/com/x62life/mo/common/util/masking/Masker.java
@@ -0,0 +1,5 @@
+package com.x62life.mo.common.util.masking;
+
+public interface Masker {
+    public String mask(String value);
+}
