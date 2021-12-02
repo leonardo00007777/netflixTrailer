bc7b4c3c1d392a4fa8fc1f136f7246fe31ad80a1
davinci000777 <Yckim2001@naver.com>
2021-12-02 10:24:37 +0900

    util - tag 추가

---
+++ src/main/java/com/x62life/mo/common/util/tags/ReplaceLineBreakTag.java
@@ -0,0 +1,46 @@
+package com.x62life.mo.common.util.tags;
+
+import java.io.IOException;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.TagSupport;
+
+public class ReplaceLineBreakTag extends TagSupport {
+	
+	static final long serialVersionUID = -5351060064721192312L;
+	private String content = "";	
+
+    public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String resultView(String content) throws JspException{
+    	
+    	String returnVal = "";
+        if (content != null) 
+        {
+                returnVal = content.replace("\r\n","<br/>");
+                returnVal = returnVal.replace("\n","<br/>");
+        }
+        return returnVal;
+    }
+    
+    @Override
+    public int doEndTag() throws JspException {
+        // TODO Auto-generated method stub
+        try{
+            JspWriter out = pageContext.getOut();
+            String contents = resultView(content);
+            out.println(contents);
+
+            return EVAL_PAGE;
+        }catch(IOException e){
+            throw new JspException();
+        }
+    }	
+}
