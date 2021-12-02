b37b14d7c85f8df534030d1a49de4177f7b3880a
davinci000777 <Yckim2001@naver.com>
2021-12-02 10:34:01 +0900

    util - viewr(pdf,xml,json,jsonp..) 추가

---
+++ src/main/java/com/x62life/mo/common/util/viewer/abstractView/JSONPView.java
@@ -0,0 +1,39 @@
+package com.x62life.mo.common.util.viewer.abstractView;
+
+import java.io.PrintWriter;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.view.AbstractView;
+
+import com.x62life.mo.common.util.StringUtil;
+
+import net.sf.json.JSONObject;
+
+public class JSONPView extends AbstractView {
+    public JSONPView() {
+        setContentType( "text/json; charset=utf-8");
+    }
+
+
+    @Override
+    protected void renderMergedOutputModel(Map<String, Object> map, HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        JSONObject jsonObject = JSONObject.fromObject(map);
+
+        response.setContentType( "text/json; charset=utf-8");
+        response.setHeader("Cache-Control", "no-cache");
+        PrintWriter out = response.getWriter();
+
+        String callback = StringUtil.getString(request.getParameter("request"), "");
+        if ("".equals(callback)){
+            out.write(callback+"("+jsonObject.toString()+")");
+        } else {
+            out.write(jsonObject.toString());
+        }
+        out.flush();
+
+    }
+}
