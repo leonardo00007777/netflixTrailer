4d36fdf9a2f4a5e8dc7ca1308c802e78bdc5ccf9
rapido1492 <rapido1492@gmail.com>
2021-12-06 15:49:52 +0900

    [20211206] model 변경

--- src/main/java/com/x62life/mo/service/main/MainService.java
+++ src/main/java/com/x62life/mo/service/main/MainService.java
@@ -3,59 +3,37 @@ package com.x62life.mo.service.main;
 import com.x62life.mo.model.boardcontents.BdContents;
 import com.x62life.mo.model.boardcontents.MagazineLEx;
 import com.x62life.mo.model.exhibition.AdMainMg;
+import com.x62life.mo.model.exhibition.MainPageSkin;
 import com.x62life.mo.model.exhibition.OneDaySpecialEx;
-import com.x62life.mo.model.product.BestProduct;
 import com.x62life.mo.model.product.GdMasterEx;
-import com.x62life.mo.model.product.SeasonalFoodHall;
-import com.x62life.mo.model.product.SpecialSellingh;
 
 import java.util.List;
 import java.util.Map;
 
 public interface MainService {
-    List<OneDaySpecialEx> oneDaySpecialList(Map<String, Object> paramMap);
+    List<AdMainMg> mainBannerList(String strMEMGRPCD);
 
-    List<AdMainMg> getSwiperBannerInfo();
+    List<MainPageSkin> renewalBannerList();
 
-    List<AdMainMg> swiperBannerRenewal();
+    List<OneDaySpecialEx> todaySpecialProdList(Map<String, Object> paramMap);
 
-    Map<String,Object> checkEventTarget(String strLoginMemCd);
+    String firstBuyGiftTargetCheck(String strMEMGRPCD);
 
-    Map<String, Object> eventTargetPayment(Map<String, Object> paramMap);
+    Map<String, Object> commonUserBuyGiftTargetCheck(Map<String, Object> paramMap);
 
-    List<OneDaySpecialEx> nowNewProdList(Map<String, Object> paramMap);
+    List<OneDaySpecialEx> nowHotProdList(Map<String, Object> paramMap);
 
-    List<GdMasterEx> newProdList (Map<String, Object> paramMap);
+    List<GdMasterEx> newProdList(Map<String, Object> paramMap);
 
-    List<SeasonalFoodHall> seasonalFoodHallList(Map<String, Object> paramMap);
+    List<GdMasterEx> bestProdList(Map<String, Object> paramMap);
 
-    List<BestProduct> bestProductList(Map<String, Object> paramMap);
+    List<GdMasterEx> discountProdList(Map<String, Object> paramMap);
 
-    List<GdMasterEx> discountProdList(String strMEMGRPCD);
+    List<Map<String, Object>> bestReviewProd(Map<String, Object> paramMap);
 
-    List<Map<String, Object>> bestReviewProdList(Map<String, Object> paramMap);
+    int magazineNum();
 
-    int magazineIdx();
-
-    List<MagazineLEx> magazineDetailList();
+    List<MagazineLEx> magazineDetail();
 
     List<BdContents> eventList(Map<String, Object> paramMap);
-
-    Map<String, Object> getRecommendListPaging();
-
-    List<GdMasterEx> getRecommendProdList(Map<String, Object> paramMap);
-
-    Map<String, Object> getRightwayListPaging();
-
-    List<GdMasterEx> getRightwayList(Map<String, Object> paramMap);
-
-    Map<String, Object> getSpecialSellingBrandListPaging(Map<String, Object> paramMap);
-
-    List<SpecialSellingh> getSpecialSellingBrandHeader(Map<String, Object> paramMap);
-
-    List<GdMasterEx> itemListBasicProdCategory(Map<String, Object> paramMap);
-
-    List<String> itemListSelectCategory(Map<String, Object> paramMap);
-
-    int itemListProdPaging(Map<String, Object> paramMap);
 }
