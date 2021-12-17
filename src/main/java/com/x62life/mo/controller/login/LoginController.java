633a69b3db78c5a29f98e44a1fba54d03a1f0797
davinci000777 <Yckim2001@naver.com>
2021-12-17 11:02:49 +0900

    mbmaster login쪽중복모델제거, bcp관련제거확인

--- src/main/java/com/x62life/mo/controller/login/LoginController.java
+++ src/main/java/com/x62life/mo/controller/login/LoginController.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.x62life.mo.common.util.TextUtil;
 import com.x62life.mo.common.util.CookiesUtil;
 import com.x62life.mo.model.member.MbMaster;
 import com.x62life.mo.service.login.LoginService;
@@ -72,7 +73,7 @@ public class LoginController {
 //        model.addAttribute("testMbrNo", memberInfo.getMbrNo());
 //
 //        //이전 페이지 주소 호출
-//        String referer = (String)userContext.getAttribute(Constants.REDIRECT_URL);
+//        String referer = (String)request.getSession().getAttribute(Constants.REDIRECT_URL);
 //
 //        if (referer == null || "".equals(referer)) {
 //            referer = RequestContext.getCurrent("baseUrl") + config.get("mainUrl");
@@ -160,34 +161,160 @@ public class LoginController {
         	CookiesUtil.setMemberInfoCookie(request, response,  memberInfo , (30 * 60),  sCookieDomain);
         	
         }else {
-        	//CookieUtil.setCookie(response, "cartTotCnt", String.valueOf(userContext.getAttribute("cartTotCnt")), (30 * 60),"/", RequestContext.getCurrent("cookieDomain"));
+        	//CookieUtil.setCookie(response, "cartTotCnt", String.valueOf(request.getSession().getAttribute("cartTotCnt")), (30 * 60),"/", RequestContext.getCurrent("cookieDomain"));
         }
 	    
     	return "login/login";
 	}
 	
-    @RequestMapping("/loginProcess")
-    public ModelAndView loginProcess(Model model, HttpServletRequest request, HttpServletResponse response){
-        ModelAndView modelAndView = new ModelAndView();
 
-        //---------------- TEST -------------------------
-        String strPassword = "ckdgysl1";
-        String strUserId = "test";
-        String memstOut = "10";
-        //------------------------------------------------
-
-        Map<String, Object> pramMap  = new HashMap<String, Object>();
-        pramMap.put("strPassword",strPassword);
-        pramMap.put("strUserId",strUserId);
-        pramMap.put("memstOut",memstOut);
-
-        MbMaster memberInfo = memberService.selectMemberInfo(pramMap);
-
-        model.addAttribute("memberInfo", memberInfo);
-
-        modelAndView.setViewName("/main/main");
-        return modelAndView;
-    }
+    /**
+     * 로그인 처리 JSON
+     *
+     * @param request
+     * @param response
+     * @return
+     * @throws Exception
+     */
+//    @RequestMapping(value = "/loginSuccess.do", method = { RequestMethod.GET, RequestMethod.POST })
+//    public String loginSuccess(@RequestParam Map<String, Object> paramMap, Model model,
+//    										HttpServletRequest request, HttpServletResponse response) throws Exception {
+//
+//        //--------------------------------
+//        // session정보 : 간편로그인
+//    	//--------------------------------
+//        // 약관동의 여부
+//        String easyLoginAgreeTerm = (String) request.getSession().getAttribute("easyLoginAgreeTerm");
+//        // 로그인 타입
+//        String easyLoginType = (String) request.getSession().getAttribute("easyLoginType");
+//        // 로그인 식별자
+//        String easyLoginIdfy = (String) request.getSession().getAttribute("easyLoginIdfy");
+//        //--------------------------------
+//        // session정보 : 회원정보
+//        //--------------------------------
+//        String memberId = (String) request.getSession().getAttribute("memberId");
+//
+//        MbMaster member = new MbMaster();
+//
+//
+//        if (!TextUtil.isEmpty(memberId)) {
+//        	
+//            member.setMemid(memberId);
+//            MbMaster custInfo = loginService.getMbrLoginInfo(customer);
+//            //MbMaster memberInfo = memberService.selectMemberInfo(pramMap);
+//            
+//
+//            //if (custInfo != null) {
+//            	//request.getSession().setAttribute("maskMbrNm", custInfo.getMaskMbrNm());
+//
+//                //----------------------------------------------
+//                // 네이버 약관동의
+//                //----------------------------------------------
+//				/*
+//				 * if("Y".equals(easyLoginAgreeTerm) &&
+//				 * Constants.EASYLOGIN_TYPE_KAKAO.equals(easyLoginType)){
+//				 * MbMaster.setMbrId(custInfo.getMbrId());
+//				 * MbMaster.setMbrNo(custInfo.getMbrNo());
+//				 * MbMaster.setEasyLoginType(easyLoginType);
+//				 * MbMaster.setEasyLoginUseYn("Y"); // gatePage에서 사용여부를 들고있어야 하기떄문에
+//				 * MbMaster.setEasyLoginFormYn("Y"); // gatePage에서 간편로그인 폼으로 인지시켜야함
+//				 * MbMaster.setEasyLoginFreePass(""); // 간편로그인 Flow 올리브영 회원 새로 만들시 기존 정보 업뎃 null
+//				 * 고정
+//				 * 
+//				 * LOGGER.debug("Oauth 약관동의후 간편로그인 연동페이지 표출"); model.addAttribute("mbrId",
+//				 * custInfo.getMbrId()); model.addAttribute("mbrNo", custInfo.getMbrNo());
+//				 * model.addAttribute("easyLoginType", MbMaster.getEasyLoginType());
+//				 * model.addAttribute("easyLoginUseYn", MbMaster.getEasyLoginUseYn());
+//				 * model.addAttribute("easyLoginFormYn", MbMaster.getEasyLoginFormYn());
+//				 * model.addAttribute("easyLoginFreePass", MbMaster.getEasyLoginFreePass());
+//				 * 
+//				 * //return "login/getEasyLoginForm"; }
+//				 */
+//
+//                //----------------------------------------------
+//                // 애플로그인 약관동의
+//                //----------------------------------------------
+//				/*
+//				 * if(!StringUtils.isEmpty(easyLoginIdfy) &&
+//				 * Constants.EASYLOGIN_TYPE_APPLE.equals(easyLoginType)) {
+//				 * 
+//				 * MbMaster.setMbrNo(custInfo.getMbrNo());
+//				 * MbMaster.setEasyLoginType(easyLoginType);
+//				 * MbMaster.setEasyLoginIdfy(easyLoginIdfy);
+//				 * MbMaster.setEasyLoginUseYn("Y");
+//				 * MbMaster.setSysRegrId(MOConsts.SYS_REGR_ID);
+//				 * MbMaster.setSysModrId(MOConsts.SYS_REGR_ID);
+//				 * custInfo.setEasyLoginType(easyLoginType);
+//				 * 
+//				 * // 동일 연동매체 동의정보가 있는지 확인 // String easyLoginUseYn =
+//				 * loginService.getEasyLoginInfo(MbMaster);
+//				 * 
+//				 * // 동일 연동매체 동의정보가 없는 경우에만 회원정보 등록 if( TextUtil.isEmpty(easyLoginUseYn) ||
+//				 * !"Y".equals(easyLoginUseYn) ) { loginService.regEasyLoginInfo(MbMaster);
+//				 * loginService.regEasyLoginChngHist(MbMaster); } }
+//				 */
+//
+//                // 로그인 성공
+//                //loginService.saveLoginSession(request.getSession(), custInfo, response); // custInfo 쿠키 생성을 위해 변경
+//
+//                //String referer = (String) request.getSession().getAttribute(Constants.REDIRECT_URL);
+//                String agreeYn = TextUtil.nvl(request.getParameter("agreeYn"), "N");
+//                String isSetting = TextUtil.nvl(request.getParameter("isSetting"), "N");
+//                String idPwSuccessYn = TextUtil.nvl(request.getParameter("idPwSuccessYn"), "N");
+//
+//                //model.addAttribute("referer", referer);
+//                model.addAttribute("agreeYn", agreeYn);
+//                model.addAttribute("memberId", member.getMemid());
+//                model.addAttribute("isSetting", isSetting);
+//
+//                //request.getSession().setAttribute("leglBdayDt", custInfo.getLeglBdayDt());
+//
+//                HashMap<String, String> resultMap = chkMbrAgesGenger(custInfo.getLeglBdayDt(), custInfo.getGenSctCd(), custInfo.getStaffYn());
+//                request.getSession().setAttribute("recoAges", resultMap.get("recoAgesStr"));
+//                request.getSession().setAttribute("genSctCd", resultMap.get("genSctCd"));
+//                request.getSession().setAttribute("staffYn", resultMap.get("staffYn"));
+//
+//
+//                // 카카오로그인 약관동의 했을 때
+//                // 통합회원 탈퇴후, 재로그인 했을 때
+//               /* if(("Y".equals(easyLoginAgreeTerm) && Constants.EASYLOGIN_TYPE_KAKAO.equals(easyLoginType)) || "Y".equals(idPwSuccessYn)){
+//                    return "login/getEasyLoginForm";
+//                }else {
+//                    return "login/addSSOCookie";
+//                }*/
+//            } else {
+//                model.addAttribute("showMessage", "회원 정보가 올바르지 않아 메인으로 이동합니다.");
+//                return "login/login";
+//            }
+//        } else {
+//            model.addAttribute("showMessage", "회원 정보가 올바르지 않아 메인으로 이동합니다.");
+//            return "login/login";
+//        }
+//    }
+	
+	
+//    @RequestMapping("/loginProcess")
+//    public ModelAndView loginProcess(Model model, HttpServletRequest request, HttpServletResponse response){
+//        ModelAndView modelAndView = new ModelAndView();
+//
+//        //---------------- TEST -------------------------
+//        String strPassword = "ckdgysl1";
+//        String strUserId = "test";
+//        String memstOut = "10";
+//        //------------------------------------------------
+//
+//        Map<String, Object> pramMap  = new HashMap<String, Object>();
+//        pramMap.put("strPassword",strPassword);
+//        pramMap.put("strUserId",strUserId);
+//        pramMap.put("memstOut",memstOut);
+//
+//        MbMaster memberInfo = memberService.selectMemberInfo(pramMap);
+//
+//        model.addAttribute("memberInfo", memberInfo);
+//
+//        modelAndView.setViewName("/main/main");
+//        return modelAndView;
+//    }
     
 // 	@RequestMapping(value="/login", method=RequestMethod.GET)
 // 	public String login(ModelMap model, HttpServletRequest request)
@@ -236,11 +363,5 @@ public class LoginController {
 	{
 		return "error";
 	}
-	
-	
-	
-	
-		
-	
 	    
 }
